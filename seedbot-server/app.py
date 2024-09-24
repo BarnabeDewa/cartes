14e62d4f6ae13fc6e7c9a369a51842a27d6f3881
BarnabeDewa <130562928+BarnabeDewa@users.noreply.github.com>
2024-09-24 12:32:52 +0100

    Update app.py

--- seedbot-server/app.py
+++ seedbot-server/app.py
@@ -106,8 +106,8 @@ def upload_file():
 
     return jsonify(coordinates)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
 
-#if __name__ == "__main__":
-   # app.run(host="0.0.0.0", port=5000)  # Permet l'accès externe à Flask
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Permet l'accès externe à Flask
